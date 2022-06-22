65fbf7439eaf4802eeedf6d32f0a05bb3fbc0afa
KlegyUnal <107717957+KlegyUnal@users.noreply.github.com>
2022-06-22 13:01:26 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,34 @@
 # from distutils.log import debug
 from urllib import request
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mysqldb import MySQL
+import Offline
 # from passlib.hash import md5_crypt as md5
 # from passlib.hash import sha256_crypt as sha256
 # from passlib.hash import sha512_crypt as sha512
 
 app = Flask(__name__)
 
-@app.route("/login", methods=["GET", "POST"])
-def index(): 
-    # data = {
-    #     "titulo": "Papelazo",
-    #     "subtitulo": "Juego",
-    #     "arreglo": ["Datos", "Piedra", "Papel", "Tijera"]
-    # }
-    return render_template("login.html")
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'papelazo'
+ 
+mysql = MySQL(app)
 
-@app.route("/acceso")
-def acceso():
-    return render_template("layout.html")
+@app.route("/")
+def index():
+    return redirect(url_for("login"))
+
+@app.route("/login", methods=["GET", "POST"])
+def login(): 
+    print("a")
+    if request.method == "POST":
+        print(request.form["username"])
+        print(request.form["password"])
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 @app.route("/registro")
 def registro():
@@ -26,7 +36,16 @@ def registro():
 
 @app.route("/menu", methods=["GET", "POST"])
 def menu():
-    return render_template("prueba.html")
+    return render_template("menu.html")
+
+@app.route("/404")
+def NoEncontrado():
+    return render_template("404.html")
+
+@app.route("/tablero")
+def offline():
+    Offline.juego()
+    return render_template("tablero.html")
 
 def qery1():
     print(request)
